7da902eac144c03487969735ea885aa128f99585
[dyad] <coo@spaceshipnft.com>
2026-02-23 22:44:37 -0300

    [dyad] Removendo widget de Primeiros Pedidos - wrote 1 file(s)

--- src/components/dashboard/DashboardLayout.tsx
+++ src/components/dashboard/DashboardLayout.tsx
@@ -3,7 +3,6 @@ import { useEffect } from "react";
 import { useUser } from "../../hooks/useUser";
 import Sidebar from "./Sidebar";
 import Header from "./Header";
-import { FirstOrdersDisplay } from "./FirstOrdersDisplay";
 
 const DashboardLayout = () => {
   const { loading, isAdmin, user } = useUser();
@@ -32,7 +31,7 @@ const DashboardLayout = () => {
   return (
     <div className="flex min-h-screen bg-gray-50">
       <Sidebar />
-      <FirstOrdersDisplay />
+      {/* Widget removido conforme solicitado */}
       <div className="flex-1 flex flex-col">
         <Header />
         <main className="p-8">
